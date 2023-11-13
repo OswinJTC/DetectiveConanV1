9a2150bc7ce3dd1ce98eab51bd5004234e7ccea4
Oswin <129711775+OswinJTC@users.noreply.github.com>
2023-11-13 17:13:44 +0800

    Update ImageController.java
    v2

--- src/main/java/com/tai/DetectiveConanV1/ImageData/ImageController.java
+++ src/main/java/com/tai/DetectiveConanV1/ImageData/ImageController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://detective-conan-client.onrender.com")
 @RequestMapping("/api/v1/images")
 public class ImageController {
 
